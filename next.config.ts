95b8e01a623d866a881e697d66a89405e2320846
John <psg6868@yahoo.com>
2025-08-22 16:30:53 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  target: 'static',
+  // or
+  rewrites: async () => {
+    return [
+      {
+        source: '/',
+        destination: '/index.html',
+      },
+    ]
+  },
+
 };
 
 export default nextConfig;
